75ffe161bf8d7c80149bab8a09865e6b9164eb5b
franciscovargas <vargfran@gmail.com>
2021-09-13 10:55:38 +0100

    Added time component to the drift

--- cfollmer/sampler_utils.py
+++ cfollmer/sampler_utils.py
@@ -6,11 +6,12 @@ class SimpleForwardNet(torch.nn.Module):
 
     def __init__(self, input_dim=1):
         super(SimpleForwardNet, self).__init__()
-
+        
+        width = 20
         self.nn = torch.nn.Sequential(
-            torch.nn.Linear(input_dim, 20), torch.nn.ReLU(),
-            torch.nn.Linear(20, 20), torch.nn.ReLU(),
-            torch.nn.Linear(20, input_dim)
+            torch.nn.Linear(input_dim + 1, width), torch.nn.ReLU(),
+            torch.nn.Linear(width, width), torch.nn.ReLU(),
+            torch.nn.Linear(width, input_dim )
         )
         
 
@@ -24,7 +25,7 @@ class FollmerSDE(torch.nn.Module):
                  batch_size=10, γ=1.0, drift=SimpleForwardNet,
                 device="cpu"):
         super().__init__()
-        self.noise_type = 'scalar'
+        self.noise_type = 'diagonal'
         self.sde_type = 'ito'
         
         self.state_size = state_size
@@ -38,8 +39,17 @@ class FollmerSDE(torch.nn.Module):
 
     # Drift
     def f(self, t, y):
-        return self.μ(y)  # shape (batch_size, state_size)
+        try:
+            d = y.shape[0] if len(y.shape) == 2 else y.shape[1]
+            t_ = t.to(self.device) * torch.ones(d,1).to(self.device)
+            t_ = t_ if len(y.shape) == 2 else t_.T[...,None]
+            y = torch.cat((y, t_), dim=-1)
+        except:
+            import pdb; pdb.set_trace()
+        return self.μ(y)   # shape (batch_size, state_size)
 
     # Diffusion
     def g(self, t, y):
-        return (torch.ones_like(y).to(self.device) * self.γ)[:, :, None]
+        kek1 = (torch.ones_like(y).to(self.device) * self.γ)  #[:, :, None]
+#         kek2 = (torch.eye(*).to(self.device) * self.γ) [:, :, None]
+        return kek1
