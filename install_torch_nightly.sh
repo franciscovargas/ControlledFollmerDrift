278330ef41f5f73f805ac10e493128a77e20f895
franciscovargas <vargfran@gmail.com>
2021-11-12 11:11:14 +0000

    install scripts updated

---
+++ install_torch_nightly.sh
@@ -0,0 +1,3 @@
+pip install ninja
+pip install --pre torch torchvision torchaudio -f https://download.pytorch.org/whl/nightly/cu113/torch_nightly.html
+pip install "git+https://github.com/facebookresearch/functorch.git"
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
+import subprocess
 try: # for pip >= 10
     from pip._internal.req import parse_requirements
 except ImportError: # for pip <= 9.0.3
@@ -8,11 +9,17 @@ except ImportError: # for pip <= 9.0.3
 
 here = path.abspath(path.dirname(__file__))
 install_reqs = parse_requirements(here + '/requirements.txt', session=False)
-reqs = [str(ir.requirement) for ir in install_reqs]
+
+try:
+    reqs = [str(ir.requirement) for ir in install_reqs]
+except:
+    reqs = [str(ir.req) for ir in install_reqs]
 
 # Get the long description from the relevant file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+subprocess.run(["bash", "install_torch_nightly.sh"])
 
 setup(
     name='ControlledFollMerDrift',
