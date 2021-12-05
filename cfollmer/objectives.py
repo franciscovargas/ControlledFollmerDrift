74ff339eee36c4aec024fa444009dc3ae3fd47f3
Francisco Vargas <5540172+franciscovargas@users.noreply.github.com>
2021-12-05 14:16:17 +0000

    STL  correct rescaling and correct vargrad

--- cfollmer/objectives.py
+++ cfollmer/objectives.py
@@ -65,7 +65,7 @@ def stl_control_cost(
 
     with torch.no_grad():
         dX = param_trajectory[1:, ...]- param_trajectory[:-1, ...]
-        dW_div_sqrt_gamma = dX - us_detached * dt
+        dW_div_sqrt_gamma = (dX - us_detached * dt) / sde.gamma
 
     # Costs
     energy_cost = torch.sum(us**2, dim=[0, 2]) * dt  / (2 * sde.gamma)
@@ -97,7 +97,7 @@ def stl_control_cost_aug(
     
     with torch.no_grad():
         dX = param_trajectory[1:, :,:-1]- param_trajectory[:-1, :,:-1]
-        dW_div_sqrt_gamma = dX - us_detached[:,:,:-1] * dt
+        dW_div_sqrt_gamma = (dX - us_detached[:,:,:-1] * dt) / sde.gamma
 
     # Costs
     energy_cost = param_T[..., -1]
@@ -118,41 +118,40 @@ def vargrad_control_cost(
         device: Optional[torch.device] = None,
     ):
     """
-    TODO : Test and double check for errors
-    
-    Note this is implemented in the most naive way following  Eq 49 (prop 3.10) in Nüsken and Ritchter
-    a more efficient implementation would augment the state-space as we disccused
-    just currently racing through this. 
-    
-    paper : https://arxiv.org/pdf/2005.05409.pdf
+    Note this is implemented in the most naive way following  Eq 49 (prop 3.10) 
+    in Nüsken and Ritchter a more efficient implementation would augment the 
+    state-space as we disccused just currently racing through this.
 
+    paper : https://arxiv.org/pdf/2005.05409.pdf
     Objective for the VarGrad (Nüsken et al. 2020) Hamilton-Bellman-Jacobi Follmer Sampler
     """
-    
+
     with torch.no_grad():
 
         param_trajectory, ts = sde.sample_trajectory(param_batch_size, dt=dt, device=device)
+        param_T = param_trajectory[-1]
     
-    param_trajectory = param_trajectory.detach()
-    param_T = param_trajectory[-1]
-
+    dX = param_trajectory[1:, ...] - param_trajectory[:-1, ...]
+    
+    # dX = v dt + \sqrt{gamma} dW
+    # u . v dt +  u .  dW \sqrt{gamma} 
+    # u . dX 
+    # (u . v dt +  u .  dW \sqrt{gamma} ) / gamma = u . v dt / gamma +   u .  dW / \sqrt{gamma}
+    # \sqrt{\gamma}  /{\gamma} = 1/\sqrt{gamma} quick sanith check
+    
     # Has shape [T, batch_size, dim]
     # Note: this is essentially evaluated twice (once before in sdeint call).
     # Could adjust torchsde to return the drift as an extra parameter (seems that there's
     # no way currently)
-    us = vmap(sde.f)(ts, param_trajectory)
-    # f_detached = lambda ts, xs: sde.f(ts, xs, detach=True)
-    us_detached = us.detach()
     
-    # dW samples for Ito integral
-    dW = torch.normal(mean=0.0, std=math.sqrt(dt), size=us_detached.shape).to(device)
+    us = vmap(sde.f)(ts, param_trajectory)
 
     # Costs
     energy_cost = torch.sum(us**2, dim=[0, 2]) * dt  / (2 * sde.gamma)
-    ito_cost = (torch.einsum("ijk,ijk->ij", us / sde.gamma, dW)).sum(axis=0) # .mean() # batched dot products
-    cross_term =  (torch.einsum("ijk,ijk->ij", us / sde.gamma, us_detached)).sum(axis=0) # .mean()
-    terminal_cost = - torch.sum(param_T**2, dim=1) / (2 * sde.gamma) - log_p(param_T)
-    
-    Y_T = energy_cost - ito_cost - cross_term
+    ito_plus_cross_term = torch.einsum("ijk,ijk->j", us , dX) / sde.gamma
 
-    return torch.var(Y_T - terminal_cost, unbiased=True)
+    terminal_cost = - torch.sum(param_T**2, dim=1) / (2 * sde.gamma) - log_p(param_T)
+
+    Y_T = energy_cost - ito_plus_cross_term
+
+    return torch.var(Y_T - terminal_cost, unbiased=True) # (N+1) denominator 
