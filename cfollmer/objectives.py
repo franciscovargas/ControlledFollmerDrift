4837a2db310fbb120e3ac0aa74fc63f953574936
Francisco Vargas <5540172+franciscovargas@users.noreply.github.com>
2021-12-08 16:20:23 +0000

    removed deprecated code.

--- cfollmer/objectives.py
+++ cfollmer/objectives.py
@@ -40,40 +40,6 @@ def relative_entropy_control_cost(
     return torch.mean(energy_cost + terminal_cost)
 
 
-def stl_control_cost(
-        sde: torch.nn.Module,
-        log_p: Callable,
-        param_batch_size: Optional[int] = 32,
-        dt: Optional[float] = 0.05,
-        device: Optional[torch.device] = None,
-    ):
-    """
-    TODO : Test and double check for errors
-    
-    Objective for the STL (Xu et al. 2021) Hamilton-Bellman-Jacobi Follmer Sampler
-    """
-
-    param_trajectory, ts = sde.sample_trajectory(param_batch_size, dt=dt, device=device)
-    param_T = param_trajectory[-1]
-
-    # Has shape [T, batch_size, dim]
-    # Note: this is essentially evaluated twice (once before in sdeint call).
-    # Could adjust torchsde to return the drift as an extra parameter (seems that there's
-    # no way currently)
-    us = vmap(sde.f)(ts, param_trajectory)
-    f_detached = lambda ts, xs: sde.f(ts, xs, detach=True)
-    us_detached = vmap(f_detached)(ts, param_trajectory)
-    
-    # dW samples for Ito integral
-    dW = torch.normal(mean=0.0, std=math.sqrt(dt), size=us_detached.shape).to(device)
-
-    # Costs
-    energy_cost = torch.sum(us**2, dim=[0, 2]) * dt  / (2 * sde.gamma)
-    ito_cost = (torch.einsum("ijk,ijk->ij", us_detached / sde.gamma, dW)).sum(axis=0).mean() # batched dot products
-    terminal_cost = - torch.sum(param_T**2, dim=1) / (2 * sde.gamma) - log_p(param_T)
-
-    return torch.mean(energy_cost + ito_cost + terminal_cost)
-
 # def vargrad_control_cost(
 #         sde: torch.nn.Module,
 #         log_p: Callable,
@@ -183,7 +149,7 @@ def stl_control_cost_aug(
 
     with torch.no_grad():
         dX = param_trajectory[1:, :,:-1] - param_trajectory[:-1, :,:-1]
-        dW_div_sqrt_gamma = dX - us_detached[:,:,:-1] * dt
+        dW_div_sqrt_gamma = (dX - us_detached[:,:,:-1] * dt) / sde.gamma
 
     # Costs
     energy_cost = param_T[..., -1]
